719a90ffc065dca5f2e73d93a01b4bde20acc335
OussamaINTI <ayari_oussama26@hotmail.fr>
2023-06-23 15:02:19 +0200

    first commit

---
+++ src/main/java/com/inti/GestionUsersV3Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsersV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsersV3Application.class, args);
+	}
+
+}
